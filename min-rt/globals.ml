95b7fc314a52a12e9191f3e63895628433d19e14
koga-yurie <ustrawblackraspsweet@g.ecc.u-tokyo.ac.jp>
2022-10-05 20:57:35 +0900

    add: 2022 version files

--- min-rt/globals.ml
+++ min-rt/globals.ml
@@ -1,85 +1,119 @@
-open MiniMLRuntime;;
+(*NOMINCAML open MiniMLRuntime;;*)
 
 (**************** グローバル変数の宣言 ****************)
 
+(* オブジェクトの個数 *)
+let n_objects = Array.make 1 0
+(*MINCAML*) in
+
 (* オブジェクトのデータを入れるベクトル（最大60個）*)
-let objects = 
+let objects =
   let dummy = Array.make 0 0.0 in
-  Array.make (60) 
-    (0, 0, 0, 0, 
-     dummy, dummy,
-     false, dummy, dummy,
-     dummy)
+  Array.make 60 (0, 0, 0, 0, dummy, dummy, false, dummy, dummy, dummy, dummy)
+(*MINCAML*) in
 
-(* [| x軸の走査線本数 , y軸の走査線本数 |] *)
-let size = Array.make 2 128
-
-(* 実行時オプション: デバッグ出力の有無 *)
-let dbg = Array.make 1 true
-(* Screen の座標 *)
+(* Screen の中心座標 *)
 let screen = Array.make 3 0.0
-(* 視点の座標 (offset なし) *)
-let vp = Array.make 3 0.0
-(* 視点の座標 (screen 位置分の offset あり) *)
-let view = Array.make 3 0.0
+(*MINCAML*) in
+(* 視点の座標 *)
+let viewpoint = Array.make 3 0.0
+(*MINCAML*) in
 (* 光源方向ベクトル (単位ベクトル) *)
 let light = Array.make 3 0.0
-(* スクリーンの回転方向: 三角関数の値で保持 *)
-let cos_v = Array.make 2 0.0
-let sin_v = Array.make 2 0.0
+(*MINCAML*) in
 (* 鏡面ハイライト強度 (標準=255) *)
 let beam = Array.make 1 255.0
+(*MINCAML*) in
 (* AND ネットワークを保持 *)
 let and_net = Array.make 50 (Array.make 1 (-1))
+(*MINCAML*) in
 (* OR ネットワークを保持 *)
 let or_net = Array.make 1 (Array.make 1 (and_net.(0)))
+(*MINCAML*) in
 
-(* reader *)
-let temp = Array.make 14 0.0 (* read_nth_object 内の作業変数 *)
-let cs_temp = Array.make 16 0.0
-
-(* solver *)
-(**** Callee との通信用グローバル変数 ****)
-(* 交点 の t の値 *)
+(* 以下、交差判定ルーチンの返り値格納用 *)
+(* solver の交点 の t の値 *)
 let solver_dist = Array.make 1 0.0
-
-(* スキャンの方向 *)
-let vscan = Array.make 3 0.0
+(*MINCAML*) in
 (* 交点の直方体表面での方向 *)
 let intsec_rectside = Array.make 1 0
+(*MINCAML*) in
 (* 発見した交点の最小の t *)
 let tmin = Array.make 1 (1000000000.0)
+(*MINCAML*) in
 (* 交点の座標 *)
-let crashed_point = Array.make 3 0.0
-(* 衝突したオブジェクト *)
-let crashed_object = Array.make 1 0
-(* 1つの AND ネットワークについての終了フラグ *)
-let end_flag = Array.make 1 false
-(* トレース開始点 *)
-let viewpoint = Array.make 3 0.0
+let intersection_point = Array.make 3 0.0
+(*MINCAML*) in
+(* 衝突したオブジェクト番号 *)
+let intersected_object_id = Array.make 1 0
+(*MINCAML*) in
 (* 法線ベクトル *)
 let nvector = Array.make 3 0.0
-(* スクリーン上の点の明るさ *)
-let rgb = Array.make 3 0.0
+(*MINCAML*) in
 (* 交点の色 *)
 let texture_color = Array.make 3 0.0
+(*MINCAML*) in
 
-(* オブジェクト中心を原点にした視点ベクトル *)
-let solver_w_vec = Array.make 3 0.0
+(* 計算中の間接受光強度を保持 *)
+let diffuse_ray = Array.make 3 0.0
+(*MINCAML*) in
+(* スクリーン上の点の明るさ *)
+let rgb = Array.make 3 0.0
+(*MINCAML*) in
 
-(* check_all_inside 用引数ベクトル *)
-let chkinside_p = Array.make 3 0.0
+(* 画像サイズ *)
+let image_size = Array.make 2 0
+(*MINCAML*) in
+(* 画像の中心 = 画像サイズの半分 *)
+let image_center = Array.make 2 0
+(*MINCAML*) in
+(* 3次元上のピクセル間隔 *)
+let scan_pitch = Array.make 1 0.0
+(*MINCAML*) in
 
-(* is_outside 用内部利用 (中心差分) ベクトル *)
-let isoutside_q = Array.make 3 0.0
+(* judge_intersectionに与える光線始点 *)
+let startp = Array.make 3 0.0
+(*MINCAML*) in
+(* judge_intersection_fastに与える光線始点 *)
+let startp_fast = Array.make 3 0.0
+(*MINCAML*) in
 
-(* グローバルに切り出したローカル変数 *)
-(* nvector *)
-let nvector_w = Array.make 3 0.0
+(* 画面上のx,y,z軸の3次元空間上の方向 *)
+let screenx_dir = Array.make 3 0.0
+(*MINCAML*) in
+let screeny_dir = Array.make 3 0.0
+(*MINCAML*) in
+let screenz_dir = Array.make 3 0.0
+(*MINCAML*) in
 
-(* main *)
-let scan_d = Array.make 1 0.0
-let scan_offset = Array.make 1 0.0
-let scan_sscany = Array.make 1 0.0
-let scan_met1 = Array.make 1 0.0
-let wscan = Array.make 3 0.0
+(* 直接光追跡で使う光方向ベクトル *)
+let ptrace_dirvec  = Array.make 3 0.0
+(*MINCAML*) in
+
+(* 間接光サンプリングに使う方向ベクトル *)
+let dirvecs =
+  let dummyf = Array.make 0 0.0 in
+  let dummyff = Array.make 0 dummyf in
+  let dummy_vs = Array.make 0 (dummyf, dummyff) in
+  Array.make 5 dummy_vs
+(*MINCAML*) in
+
+(* 光源光の前処理済み方向ベクトル *)
+let light_dirvec =
+  let dummyf2 = Array.make 0 0.0 in
+  let v3 = Array.make 3 0.0 in
+  let consts = Array.make 60 dummyf2 in
+  (v3, consts)
+(*MINCAML*) in
+
+(* 鏡平面の反射情報 *)
+let reflections =
+  let dummyf3 = Array.make 0 0.0 in
+  let dummyff3 = Array.make 0 dummyf3 in
+  let dummydv = (dummyf3, dummyff3) in
+  Array.make 180 (0, dummydv, 0.0)
+(*MINCAML*) in
+
+(* reflectionsの有効な要素数 *)
+let n_reflections = Array.make 1 0
+(*MINCAML*) in
--- min-rt/miniMLRuntime.ml
+++ min-rt/miniMLRuntime.ml
@@ -1,25 +1,75 @@
-let print_byte x = Pervasives.print_char (char_of_int x)
-let prerr_byte x = Pervasives.prerr_char (char_of_int x)
+(* float (1) *)
+external fequal : float -> float -> bool = "%equal"
+external fless : float -> float -> bool = "%lessthan"
+
+let fispos x = x > 0.0
+let fisneg x = x < 0.0
+let fiszero x = (x = 0.0)
+
+(* int *)
+external (=) : int -> int -> bool = "%equal"
+external (<>) : int -> int -> bool = "%notequal"
+external (<) : int -> int -> bool = "%lessthan"
+external (>) : int -> int -> bool = "%greaterthan"
+external (<=) : int -> int -> bool = "%lessequal"
+external (>=) : int -> int -> bool = "%greaterequal"
+
+external (+) : int -> int -> int = "%addint"
+external (-) : int -> int -> int = "%subint"
+external ( * ) : int -> int -> int = "%mulint"
+external (/) : int -> int -> int = "%divint"
+
+(* logic *)
+external xor : bool -> bool -> bool = "%notequal"
+external not : bool -> bool = "%boolnot"
+
+(* float (2) *)
+let fhalf x = x *. 0.5
+let fsqr x = x *. x
+external (+.) : float -> float -> float = "%addfloat"
+external (-.) : float -> float -> float = "%subfloat"
+external ( *. ) : float -> float -> float = "%mulfloat"
+external (/.) : float -> float -> float = "%divfloat"
+
+external fabs : float -> float = "%absfloat"
+external fneg : float -> float = "%negfloat"
+external sqrt : float -> float = "sqrt_float" "sqrt" [@@unboxed] [@@noalloc] (* "float" *)
+external floor : float -> float = "floor_float" "floor" [@@unboxed] [@@noalloc] (* "float" *)
+
+external int_of_float : float -> int = "%intoffloat"
+external float_of_int : int -> float = "%floatofint"
+
+external cos : float -> float = "cos_float" "cos" [@@unboxed] [@@noalloc] (* "float" *)
+external sin : float -> float = "sin_float" "sin" [@@unboxed] [@@noalloc] (* "float" *)
+external atan : float -> float = "atan_float" "atan" [@@unboxed] [@@noalloc] (* "float" *)
+
+external int_of_float : float -> int = "%intoffloat"
+external float_of_int : int -> float = "%floatofint"
+
+(* external create_array : int -> 'a -> 'a array = "make_vect" *)
+
+(* I/O *)
+
+let print_char x = Stdlib.print_char (char_of_int x)
+let print_int = Stdlib.print_int
 
 let buf = Buffer.create 16
 
-(* let chan = open_in_bin "piero1.sld" *) (* for ocamldebug *)
-
 let rec read_token in_token =
   try
-    let c = input_char stdin (* chan *) in
+    let c = input_char stdin in
     match c with
       ' ' | '\t' | '\r' | '\n' ->
-        if in_token then ()
-        else read_token false
+	if in_token then ()
+	else read_token false
     | _ ->
-        Buffer.add_char buf c;
-        read_token true
+	Buffer.add_char buf c;
+	read_token true
   with
     End_of_file ->
       if in_token then () else raise End_of_file
 
-let read_float () = 
+let read_float () =
   Buffer.clear buf;
   read_token false;
   try
@@ -27,7 +77,7 @@ let read_float () =
   with
     Failure _ -> failwith ((Buffer.contents buf) ^ ": float conversion failed.")
 
-let read_int () = 
+let read_int () =
   Buffer.clear buf;
   read_token false;
   try
