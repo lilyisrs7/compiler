bed71acd29423811d5a5c3fcc627b9b0b2b38d9c
koga-yurie <ustrawblackraspsweet@g.ecc.u-tokyo.ac.jp>
2022-10-28 16:02:59 +0900

    add: sin

--- library.ml
+++ library.ml
@@ -15,3 +15,14 @@ let cos x =
 let atan x =
 let floor x =
 *)
+let pi = 3.14159265358979323846264 in
+let rec sin x =
+  let pi2 = pi *. 2.0 in
+  let pihalf = pi *. 0.5 in
+  if x < 0 then sin (x +. pi2)
+  else if x > pi2 then sin (x -. pi2)
+  else if x > pi then -. 1.0 * (sin (x -. pi))
+  else if x > pihalf then sin (pi -. x)
+  else
+    let t = tan (x *. 0.5) in
+    (2.0 * t) / (1.0 +.)
