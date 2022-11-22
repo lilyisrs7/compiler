e5cdb31fb0e1b5a479ebba7cc648a0b6bed1f209
koga-yurie <ustrawblackraspsweet@g.ecc.u-tokyo.ac.jp>
2022-11-22 13:44:37 +0900

    add: print_newline

--- library.ml
+++ library.ml
@@ -1,3 +1,4 @@
+let rec print_newline _ = print_char 10 in
 let rec fiszero x = (x = 0.0) in
 let rec fispos x = (x > 0.0) in
 let rec fisneg x = (x < 0.0) in
@@ -6,9 +7,15 @@ let rec fabs x = if x > 0.0 then x else -.x in
 let rec fless x y = (x < y) in
 let rec fhalf x = x *. 0.5 in
 let rec fsqr x = x *. x in
+let rec int_of_float x =
+  if x >= 0.0 then
+    if x >= 1.0 then int_of_float (x -. 1.0) + 1 else 0
+  else - (int_of_float (-.x)) in
+let rec float_of_int x =
+  if x >= 0 then
+    if x >= 1 then float_of_int (x - 1) +. 1.0 else 0.0
+  else -. (float_of_int (-x)) in
 let rec floor x = float_of_int (int_of_float x) in
-let rec int_of_float x = if x >= 1.0 then int_of_float (x -. 1.0) + 1 else 0 in
-let rec float_of_int x = if x >= 1 then float_of_int (x - 1) +. 1.0 else 0.0 in
 let pi = 3.14159265358979323846264 in
 let pi2 = pi *. 2.0 in
 let pihalf = pi *. 0.5 in
@@ -23,24 +30,29 @@ let piquat = pi *. 0.25 in
   else
     let t = tan (x *. 0.5) in
     (2.0 * t) / (1.0 +.)*)
+
+let rec sin_main x =
+  let x2 = x *. x in
+  let x3 = x *. x2 in
+  x -. 0.16666666666666666666666 *. x3 +. 0.00833333333333333333333 *. x2 *. x3 -. 0.00019841269841269841269 *. x2 *. x2 *. x3 in
+
+let rec cos_main x =
+  let x2 = x *. x in
+  let x4 = x2 *. x2 in
+  1.0 -. 0.5 *. x2 +. 0.04166666666666666666666 *. x4 -. 0.00138888888888888888888 *. x2 *. x4 in
+
 let rec sin x =
   if x < 0.0 then sin (x +. pi2)
   else if x > pi2 then sin (x -. pi2)
   else if x > pi then -. 1.0 *. (sin (x -. pi))
   else if x > pihalf then sin (pi -. x)
-  else if x > piquat then cos (pihalf -. x)
-  else
-    let x2 = x *. x in
-    let x3 = x *. x2 in
-    x -. 0.16666666666666666666666 *. x3 +. 0.00833333333333333333333 *. x2 *. x3 -. 0.00019841269841269841269 *. x2 *. x2 *. x3 in
+  else if x > piquat then cos_main (pihalf -. x)
+  else sin_main x in
 
 let rec cos x =
   if x < 0.0 then sin (x +. pihalf)
   else if x > piquat then sin (pihalf -. x)
-  else
-    let x2 = x *. x in
-    let x4 = x2 *. x2 in
-    1.0 -. 0.5 *. x2 +. 0.04166666666666666666666 *. x4 -. 0.00138888888888888888888 *. x2 *. x4 in
+  else cos_main x in
 
 let rec atan x =
   if x < 0.0 then -. atan (-. x)
