c3581099243c2cb4f1aae9ef0ebd68b8d248195e
koga-yurie <ustrawblackraspsweet@g.ecc.u-tokyo.ac.jp>
2022-11-25 20:48:23 +0900

    change: fabs

--- library.ml
+++ library.ml
@@ -2,7 +2,7 @@ let rec fiszero x = (x = 0.0) in
 let rec fispos x = (x > 0.0) in
 let rec fisneg x = (x < 0.0) in
 let rec fneg x = -.x in
-let rec fabs x = if x >= 0.0 then x else -.x in
+let rec fabs x = if x > 0.0 then x else -.x in
 let rec fless x y = (x < y) in
 let rec fhalf x = x *. 0.5 in
 let rec fsqr x = x *. x in
