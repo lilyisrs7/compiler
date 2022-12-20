d4b3d227ba3b635197378e7a113e40f002070719
koga-yurie <ustrawblackraspsweet@g.ecc.u-tokyo.ac.jp>
2022-12-20 16:29:50 +0900

    change: not to inline recursive functions

--- inline.ml
+++ inline.ml
@@ -14,7 +14,7 @@ let rec g env = function (* インライン展開ルーチン本体 (caml2html: inline_g) *)
   | IfLE(x, y, e1, e2, pos) -> IfLE(x, y, g env e1, g env e2, pos)
   | Let(xt, e1, e2, pos) -> Let(xt, g env e1, g env e2, pos)
   | LetRec({ name = (x, t); args = yts; body = e1 }, e2, pos) -> (* 関数定義の場合 (caml2html: inline_letrec) *)
-      if S.mem x (fv e1) then (* 再帰関数はインライン展開しない *)
+      if S.mem x (fv e1) then (* 再帰関数は展開しない *)
         LetRec({ name = (x, t); args = yts; body = g env e1}, g env e2, pos)
       else
         let env = if size e1 > !threshold then env else M.add x (yts, e1) env in
