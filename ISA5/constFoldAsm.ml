543e19e440a99f7afbb5cc801edd98fe9202a515
koga-yurie <ustrawblackraspsweet@g.ecc.u-tokyo.ac.jp>
2023-02-10 02:52:40 +0900

    add: float opt in constfoldasm

--- ISA5/constFoldAsm.ml
+++ ISA5/constFoldAsm.ml
@@ -1,7 +1,7 @@
 (* 配列アドレスが定数に *)
 open Asm
 
-let datamap = ref M.empty
+let datamap = ref []
 
 let memi x env =
   try (match M.find x env with Set(_) -> true | _ -> false)
@@ -16,7 +16,7 @@ let memi' x env =
   | C(_) -> true
 
 let findi x env = (match M.find x env with Set(i, _) -> i | _ -> raise Not_found)
-let findf x env = (match M.find x env with SetL(Id.L(x), _) -> M.find x !datamap | _ -> raise Not_found)
+let findf x env = (match M.find x env with SetL(Id.L(x), _) -> x | _ -> raise Not_found)
 
 let findi' x env =
   match x with
@@ -49,19 +49,82 @@ and g' env = function
   | Sub(x, y, id) when memi x env && memi y env -> Format.eprintf "sub %s %s\n" x y; Ans(Set(findi x env - findi y env, id), -1)
   | Mul(x, y, id) when memi x env && memi y env -> Format.eprintf "mul %s %s\n" x y; Ans(Set(findi x env * findi y env, id), -1)
   | Div(x, y, id) when memi x env && memi y env -> Format.eprintf "div %s %s\n" x y; Ans(Set(findi x env / findi y env, id), -1)
-  (* | FMov(x, id) when memf x env -> SetL(findf x env, id)
-  | FNeg(x, id) when memf x env -> SetL(-.(findf x env), id)
-  | FAdd(x, y, id) when memf x env && memf y env -> Float(findf x env +. findf y env, id)
-  | FSub(x, y, id) when memf x env && memf y env -> Float(findf x env -. findf y env, id)
-  | FMul(x, y, id) when memf x env && memf y env -> Float(findf x env *. findf y env, id)
-  | FDiv(x, y, id) when memf x env && memf y env -> Float(findf x env /. findf y env, id) *)
-  | IfEq(x, y, e1, e2, id) when memi x env && memi y env -> Format.eprintf "ifeq %s %s\n" x y; if findi x env = findi y env then g env e1 else g env e2
+  | FMovD(x, id) when memf x env -> Format.eprintf "fmovd %s\n" x; Ans(SetL(Id.L(findf x env), id), -1)
+  | FNegD(x, id) when memf x env ->
+      Format.eprintf "fnegd %s\n" x;
+      let v = -.(List.assoc (findf x env) !datamap) in
+      let l =
+        try
+          let (l, _) = List.find (fun (_, d) -> v = d) !datamap in l
+        with Not_found ->
+          let l = Id.genid "l" in
+          datamap := (l, v) :: !datamap;
+          l in
+      Ans(SetL(Id.L(l), id), -1)
+  | FAddD(x, y, id) when memf x env && memf y env ->
+      Format.eprintf "faddd %s %s\n" x y;
+      let v = List.assoc (findf x env) !datamap +. List.assoc (findf y env) !datamap in
+      let l =
+        try
+          let (l, _) = List.find (fun (_, d) -> v = d) !datamap in l
+        with Not_found ->
+          let l = Id.genid "l" in
+          datamap := (l, v) :: !datamap;
+          l in
+      Ans(SetL(Id.L(l), id), -1)
+  | FSubD(x, y, id) when memf x env && memf y env ->
+      Format.eprintf "fsubd %s %s\n" x y;
+      let v = List.assoc (findf x env) !datamap -. List.assoc (findf y env) !datamap in
+      let l =
+        try
+          let (l, _) = List.find (fun (_, d) -> v = d) !datamap in l
+        with Not_found ->
+          let l = Id.genid "l" in
+          datamap := (l, v) :: !datamap;
+          l in
+      Ans(SetL(Id.L(l), id), -1)
+  | FMulD(x, y, id) when memf x env && memf y env ->
+      Format.eprintf "fmuld %s %s\n" x y;
+      let v = List.assoc (findf x env) !datamap *. List.assoc (findf y env) !datamap in
+      let l =
+        try
+          let (l, _) = List.find (fun (_, d) -> v = d) !datamap in l
+        with Not_found ->
+          let l = Id.genid "l" in
+          datamap := (l, v) :: !datamap;
+          l in
+      Ans(SetL(Id.L(l), id), -1)
+  | FDivD(x, y, id) when memf x env && memf y env ->
+      Format.eprintf "fdivd %s %s\n" x y;
+      let v = List.assoc (findf x env) !datamap /. List.assoc (findf y env) !datamap in
+      let l =
+        try
+          let (l, _) = List.find (fun (_, d) -> v = d) !datamap in l
+        with Not_found ->
+          let l = Id.genid "l" in
+          datamap := (l, v) :: !datamap;
+          l in
+      Ans(SetL(Id.L(l), id), -1)
+  | Sqrt(x, id) when memf x env ->
+      Format.eprintf "sqrt %s\n" x;
+      let v = sqrt (List.assoc (findf x env) !datamap) in
+      let l =
+        try
+          let (l, _) = List.find (fun (_, d) -> v = d) !datamap in l
+        with Not_found ->
+          let l = Id.genid "l" in
+          datamap := (l, v) :: !datamap;
+          l in
+      Ans(SetL(Id.L(l), id), -1)
+  | IfEq(x, y, e1, e2, id) when memi x env && memi y env -> if findi x env = findi y env then g env e1 else g env e2
   | IfEq(x, y, e1, e2, id) -> Ans(IfEq(x, y, g env e1, g env e2, id), -1)
-  | IfLE(x, y, e1, e2, id) when memi x env && memi y env -> Format.eprintf "ifle %s %s\n" x y; if findi x env <= findi y env then g env e1 else g env e2
+  | IfLE(x, y, e1, e2, id) when memi x env && memi y env -> if findi x env <= findi y env then g env e1 else g env e2
   | IfLE(x, y, e1, e2, id) -> Ans(IfLE(x, y, g env e1, g env e2, id), -1)
-  | IfFEq(x, y, e1, e2, id) when memf x env && memf y env -> Format.eprintf "iffeq %s %s\n" x y; if findf x env = findf y env then g env e1 else g env e2
+  | IfFEq(x, y, e1, e2, id) when memf x env && memf y env ->
+      if List.assoc (findf x env) !datamap = List.assoc (findf y env) !datamap then g env e1 else g env e2
   | IfFEq(x, y, e1, e2, id) -> Ans(IfFEq(x, y, g env e1, g env e2, id), -1)
-  | IfFLE(x, y, e1, e2, id) when memf x env && memf y env -> Format.eprintf "iffle %s %s\n" x y; if findf x env <= findf y env then g env e1 else g env e2
+  | IfFLE(x, y, e1, e2, id) when memf x env && memf y env ->
+      if List.assoc (findf x env) !datamap <= List.assoc (findf y env) !datamap then g env e1 else g env e2
   | IfFLE(x, y, e1, e2, id) -> Ans(IfFLE(x, y, g env e1, g env e2, id), -1)
   | e -> Ans(e, -1)
 
@@ -69,7 +132,8 @@ let h { name = l; args = ys; fargs = zs; body = e; ret = t } =
   { name = l; args = ys; fargs = zs; body = g (M.add "x30" (Set(4, -1)) M.empty) e; ret = t }
 
 let f (Prog(data, fundefs, e)) =
-  List.iter (fun (Id.L(x), f) -> datamap := M.add x f !datamap) data;
+  datamap := List.map (fun (Id.L(x), f) -> (x, f)) data;
   let fundefs' = List.map h fundefs in
   let e' = g (M.add "x30" (Set(4, -1)) M.empty) e in
-  Prog(data, fundefs', e')
+  let data' = List.map (fun (x, f) -> (Id.L(x), f)) !datamap in
+  Prog(data', fundefs', e')
